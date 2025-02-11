916849af58572cbf223d6bae046d26ba68200a1c
programmaraditya123 <officialadi4255@gmail.com>
2025-02-11 17:56:25 +0530

    commit by aditya

--- frontend/src/Pages/Tutorial.js
+++ frontend/src/Pages/Tutorial.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../Styles/Tutorial.css';
 import LowerFooter from '../Components/LowerFooter';
+import a from '../images/a.jpeg'; 
 
 const Tutorial = () => {
   return (
@@ -55,7 +56,7 @@ const Tutorial = () => {
       <div className='ctutbody'> 
       <div className='ctutbodyitems'>
         <p>Python Tutorial | Learn Python Programming Language</p>
-        <p>Last Updated : 28 Jan, 2025</p>
+        <h4>Last Updated : 28 Jan, 2025</h4>
         <hr></hr>
         <div className='cont1'>
           <p>Python Tutorial - Python is one of the most popular programming languages today, known for its simplicity, extensive features and library support. Its clean and straightforward syntax makes it beginner-friendly, while its powerful libraries and frameworks makes it perfect for developers.</p>
@@ -65,26 +66,10 @@ const Tutorial = () => {
           Used in various fields like web development, data science, artificial intelligence and automation.</li>
         </div>
         <div className='cont2'>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel deserunt quisquam cumque aliquam dolore at quibusdam cupiditate. Qui dolorem nihil facilis deleniti aliquam vero maxime! Animi voluptatum fuga magni sit!
-          Modi eligendi, accusantium obcaecati officia repellat exercitationem corrupti esse, nobis sed quibusdam iure consequuntur tenetur aliquid eos veritatis! Libero, mollitia repellat voluptate unde id sapiente incidunt voluptas itaque similique voluptates.
-          Eius iusto similique non fuga distinctio perspiciatis libero tenetur cumque architecto voluptatum vero laborum, dignissimos reiciendis atque expedita molestias! Veniam dolorum rem nulla hic. Vero possimus maxime debitis et rerum?
-          Provident repellat magni harum esse nobis, nam eius dolorem aliquid neque commodi fugit, amet labore optio ex quas. Quis dolores repudiandae soluta sint esse porro sed molestiae veniam quidem nobis!
-          Quisquam corporis aperiam adipisci nam quod ad, possimus officia enim molestiae dolorem, soluta praesentium? Aperiam consectetur voluptatum, aliquam illo id nemo iusto dolores, magni animi mollitia perferendis perspiciatis nostrum ratione!
-          Ipsa, accusantium voluptatem iste mollitia odio, at voluptatibus quam, quasi ratione labore molestiae soluta sapiente nesciunt reprehenderit consectetur? Adipisci repellat cupiditate suscipit veniam culpa inventore magnam perferendis perspiciatis dolore esse?
-          Reiciendis, architecto! Saepe laudantium rerum rem temporibus dolores non impedit, id qui! Culpa, optio. Vitae iusto vero enim provident aspernatur corrupti nam quaerat quasi distinctio maiores! Inventore asperiores harum sapiente?
-          Consequatur debitis eum, dicta enim laborum repellendus voluptate cumque necessitatibus earum facilis neque delectus quas, distinctio natus sint voluptatum modi nam voluptas? Culpa quaerat facilis iusto temporibus voluptatibus odio? Aut.
-          Itaque accusantium ullam expedita saepe deleniti, quo quasi dolorum. Ea commodi, nam praesentium modi veniam repellat numquam culpa, neque sint error pariatur fuga suscipit alias labore illo quasi at inventore.
-          Sunt sed, modi delectus corrupti natus dolorem aperiam maiores eum inventore vero excepturi iusto! Tempora omnis rerum nesciunt fuga tempore. Suscipit, reprehenderit ad similique asperiores ut debitis aperiam vel consectetur!
-          Sunt architecto aspernatur, iusto, eligendi facere incidunt tempora veniam magni dignissimos numquam dolorem ipsam. Eos aspernatur necessitatibus laborum quisquam nesciunt explicabo. Ea doloremque illum assumenda quidem iste et nesciunt explicabo.
-          Quae, cumque debitis minima, reprehenderit illo dolore consequuntur sequi impedit fugiat omnis nemo provident esse officia aperiam placeat, modi voluptate. Optio quis mollitia necessitatibus suscipit commodi blanditiis, aperiam labore pariatur.
-          Quod esse, numquam harum nostrum incidunt natus maiores molestias nemo provident sit totam? Veniam nemo cumque vero possimus voluptates! Maxime iure nisi delectus. Repellendus sed odio dolor facere illum voluptate?
-          Aliquam tempore dolor vitae. Dolorem, eum nemo voluptas provident obcaecati veritatis illo temporibus, ullam ratione quis voluptate asperiores numquam ipsum distinctio sunt eligendi qui quas ex dolores praesentium quod sapiente.
-          Cumque sint error obcaecati quisquam non velit fuga consectetur, voluptatibus dicta rem ipsam, dolore at animi eos expedita sed tempore, corporis quas porro explicabo. Eos numquam pariatur tempore minus quae?
-          Perferendis consequatur possimus quibusdam facere officia, consectetur nisi excepturi! Asperiores modi reprehenderit impedit eligendi similique vel corrupti, optio deserunt debitis sed aspernatur dolore maxime nobis possimus praesentium sapiente, quod fugit!
-          Recusandae, cum aspernatur at id qui nemo optio beatae, quam minus vel obcaecati. Odio cum quaerat ipsum mollitia delectus veniam officia, dolorum exercitationem asperiores, dolore repellendus accusamus optio. Asperiores, magnam.
-          Quod harum eius vitae veniam quia. Earum, sit? Aut reprehenderit ipsum labore. Voluptatem vel tenetur reprehenderit eaque maiores, vitae soluta maxime quo eius, quia, optio ullam omnis. Dicta, cumque? Officiis!
-          Alias, dolorum incidunt ex quos, vel at expedita quibusdam odio, molestiae eaque rem a eum eveniet in provident eius. Cupiditate perferendis non alias eligendi unde, quae ducimus mollitia maxime omnis.
-          Libero dolore nesciunt voluptate quas. Eius molestiae, ab beatae deleniti architecto ullam nostrum earum. Mollitia, perferendis aliquam quod repellendus beatae quia rem dignissimos sit, eos tenetur atque voluptatem vero! Assumenda!
+        <img src={a} alt='logo'></img>
+        <h2>First Python Program</h2>
+        <p>Here is a simple Python code, printing a string. We recommend we to edit the code and try to print our own name.</p>
+              
         </div>
       </div>
       </div>
